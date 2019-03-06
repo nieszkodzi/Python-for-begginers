fe9936f69e624c5ccf8338fd5d7ea5e9ead43c4d
Natalia Ziemba-Jankowska <43490893+nieszkodzi@users.noreply.github.com>
2019-03-07 00:37:28 +0100

    Add files via upload

---
+++ day.py
@@ -0,0 +1,3 @@
+from datetime import date
+print('Today is', date.today().strftime('%A'),'. Have a nice day!')
+input('Press ENTER to continue')
---
+++ script test.py
@@ -0,0 +1,7 @@
+#!/home/natalia/anaconda3/bin//python
+
+
+print('Wypisz tutaj jakiś banał.')
+print('Jescze coś, żeby nie było nudno.')
+
+input("Naciśnij enter żeby kontynuować.")
